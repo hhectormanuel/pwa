b999821491095e63949512592797c6c3d8dca85f
hector <hectorcena@live.com.mx>
2022-11-25 21:48:26 -0700

    api and location permissions created

--- djangopwa/views.py
+++ djangopwa/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+ 
+
 # Create your views here.
 
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
+        print("entra aqui")
+        print(request.method)
+        print(request.GET.get("nose"))
         context = {}
         return render(request, 'index3.html', context)
